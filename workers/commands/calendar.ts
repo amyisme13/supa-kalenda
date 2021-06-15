e5e92c7705c76ff421ed41288c49f435ea9cc488
Azmi Makarima <amy.azmim@gmail.com>
2021-06-15 15:04:12 +0700

    fix: fix heroku error & sometimes events not loaded

--- workers/commands/calendar.ts
+++ workers/commands/calendar.ts
@@ -4,20 +4,19 @@ import config from '../config';
 import { CommandFn } from '../utils/telegraf';
 
 const takeScreenshot = async () => {
-  console.log('start screenshot');
   const browser = await chromium.launch({
     chromiumSandbox: false,
     args: ['--disable-gpu'],
   });
-  console.log('chrome launched');
+
   const page = await browser.newPage();
-  console.log('new page opened');
-  await page.goto(`${config.url}?noauth=1`);
-  console.log('page navigated');
+  await Promise.all([
+    page.waitForResponse((res) => res.url().includes('/users')),
+    page.waitForResponse((res) => res.url().includes('/events')),
+    page.goto(`${config.url}?noauth=1`),
+  ]);
   const screenshot = await page.screenshot({ type: 'png' });
-  console.log('screenshot created');
   await browser.close();
-  console.log('browser closed');
 
   return screenshot;
 };
@@ -28,9 +27,7 @@ const calendar: CommandFn = async (ctx) => {
   const screenshot = await takeScreenshot();
 
   await ctx.replyWithPhoto({ source: screenshot });
-  console.log('photo replied');
   await ctx.deleteMessage(message.message_id);
-  console.log('message deleted');
 };
 
 export default calendar;
